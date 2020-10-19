bb9eb3d3c004726bcff4d42c399cf81d8bd07610
Queenie <jiyuqi0310@gmail.com>
2020-10-19 12:22:00 +1100

    Update Index.vue

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,86 +1,92 @@
 <template>
-  <div>
-    <div class="outer">
-      <div class="left">
-        <img class="logo" src="../assets/bigger_logo.png" alt="logo">
-      </div>
-      <div class="right">
-        <div class="words">
-          <h1> Aauth </h1>
-          <h2> Auth with anything</h2>
-        </div>
-        <v-btn
-          depressed
-          rounded
-          large
-          color="primary"
-          style="margin-right: 40px">
-          GET STARTED
-        </v-btn>
-        <v-btn
-          depressed
-          rounded
-          large
-          color="secondary">
-          GitHub
-        </v-btn>
-      </div>
+  <div class="index">
+    <img src="../assets/bigger_logo.png" alt="logo">
+    <h2 :class="{ fold: hide }">Auth with anything</h2>
+    <div class="button" :class="{ fold: hide }">
+      <v-btn
+        rounded
+        large
+        dark
+        color="#D9483B"
+        style="margin: 0 10px;">
+        GET STARTED
+      </v-btn>
+      <v-btn
+        rounded
+        large
+        color="#f6f6f6"
+        style="margin: 0 10px;"
+        @click="github">
+        <img style="width: 25px; margin-right: 20px;" src="/platforms/github.png">
+        GitHub
+      </v-btn>
     </div>
-  <div class="footer">
-    <a id="href" href="http://beian.miit.gov.cn/" target="_blank">苏ICP备20040754号-1</a>
-  </div>
+    <a href="http://beian.miit.gov.cn/" style="color: #e4e6eb;">苏ICP备20040754号-1</a>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Index'
+  name: 'Index',
+  data: () => ({
+    hide: true
+  }),
+  mounted () {
+    setTimeout(() => {
+      this.hide = false
+    }, 1000)
+  },
+  methods: {
+    github () {
+      window.location.href = 'https://github.com/yzITI/matrix-fe'
+    }
+  }
 }
 </script>
 
 <style scoped>
-.outer {
-  max-width: 1260px;
-  margin: 130px auto;
-  height: 100%;
+.index {
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
 }
-.logo {
-  width: 300px;
-  float: right;
-  margin-right: 60px;
-}
-.left {
-  width: 43%;
-  display: inline-block;
-  vertical-align: top;
-}
-.right {
-  width: 57%;
-  display: inline-block;
-  vertical-align: top;
-}
-.words {
-  display: block;
-  color: #2a3d50;
-}
-h1{
-  font-weight: 400;
-  font-size: 5.2em;
+div.index * {
+  transition: all 0.5s ease;
+  overflow-y: hidden;
 }
 h2 {
-  font-weight: 300;
+  height: 76px;
+  color: #273849;
   font-size: 3.2em;
+  font-weight: 300;
+  margin-top: 0;
 }
-.footer {
+img {
+  width: 40vw;
+  max-width: 250px;
+}
+a {
   position: fixed;
   bottom: 0;
-  left: 45%;
-  margin-right: -50%;
-  display: block;
-}
-#href {
+  width: 100%;
   text-decoration: none;
-  font-weight: 200;
-  color:#999;
+}
+.button {
+  height: 50px;
+  display: flex;
+  margin: 40px;
+}
+@media (max-width: 420px) {
+  h2 {
+    font-size: 2rem;
+    height: 60px;
+  }
+}
+.fold {
+  height: 0;
 }
 </style>
